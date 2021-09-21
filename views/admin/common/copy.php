84d2a817f8a1f3ae23a6d9e8f620b7e72a78f166
Dernerd <dern3rd@outlook.at>
2021-09-21 18:30:29 +0200

    Anpassung

--- views/admin/common/copy.php
+++ views/admin/common/copy.php
@@ -2,7 +2,7 @@
 $dialog_id = sprintf( 'pstoolkit-copy-settings-%s', $module['module'] );
 $data_name = '_' . PSToolkit_Helper::hyphen_to_underscore( $dialog_id );
 ?>
-<button type="button" class="sui-button sui-button-ghost" data-modal-open="<?php echo esc_attr( $dialog_id ); ?>" data-modal-mask="true" data-data-name="<?php echo esc_attr( $data_name ); ?>" ><?php echo esc_html_x( 'Copy Settings', 'button', 'ub' ); ?></button>
+<button type="button" class="sui-button sui-button-ghost" data-modal-open="<?php echo esc_attr( $dialog_id ); ?>" data-modal-mask="true" data-data-name="<?php echo esc_attr( $data_name ); ?>" ><?php echo esc_html_x( 'Kopiere Einstellungen', 'button', 'ub' ); ?></button>
 <div class="sui-modal sui-modal-sm">
 	<div class="sui-modal-content" id="<?php echo esc_attr( $dialog_id ); ?>" aria-labelledby="<?php echo esc_attr( $dialog_id ) . '-title'; ?>" aria-describedby="<?php echo esc_attr( $dialog_id ) . '-description'; ?>" role="dialog" aria-modal="true">
 		<div class="sui-box" role="document">
@@ -11,14 +11,14 @@ $data_name = '_' . PSToolkit_Helper::hyphen_to_underscore( $dialog_id );
 					<i class="sui-icon-close sui-md" aria-hidden="true"></i>
 					<span class="sui-screen-reader-text"><?php esc_html_e( 'Schließe dieses Modal', 'ub' ); ?></span>
 				</button>
-				<h3 class="sui-box-title sui-lg" id="<?php echo esc_attr( $dialog_id ) . '-title'; ?>"><?php esc_html_e( 'Copy Settings', 'ub' ); ?></h3>
+				<h3 class="sui-box-title sui-lg" id="<?php echo esc_attr( $dialog_id ) . '-title'; ?>"><?php esc_html_e( 'Kopiere Einstellungen', 'ub' ); ?></h3>
 			</div>
 			<div class="sui-box-body sui-content-center sui-flatten">
-				<p id="<?php echo esc_attr( $dialog_id ) . '-description'; ?>"><?php esc_html_e( 'Choose the module you want to copy settings from and specify the sections to copy the setting of.', 'ub' ); ?></p>
+				<p id="<?php echo esc_attr( $dialog_id ) . '-description'; ?>"><?php esc_html_e( 'Wähle das Modul aus, von dem Du die Einstellungen kopieren möchtest, und gib die Abschnitte an, von denen die Einstellungen kopiert werden sollen.', 'ub' ); ?></p>
 				<div class="sui-form-field">
-					<label for="dialog-text-5" class="sui-label"><?php esc_html_e( 'Copy settings from', 'ub' ); ?></label>
+					<label for="dialog-text-5" class="sui-label"><?php esc_html_e( 'Einstellungen kopieren von', 'ub' ); ?></label>
 					<select class="pstoolkit-copy-settings-select">
-						<option value=""><?php esc_html_e( 'Choose module', 'ub' ); ?></option>
+						<option value=""><?php esc_html_e( 'Modul wählen', 'ub' ); ?></option>
 <?php
 asort( $related );
 foreach ( $related as $module_key => $data ) {
@@ -51,8 +51,8 @@ foreach ( $related as $module_key => $data ) {
 ?>
 			</div>
 			<div class="sui-box-footer sui-space-between sui-flatten">
-				<button type="button" class="sui-button sui-button-ghost" data-modal-close=""><?php echo esc_html_x( 'Cancel', 'Dialog "Copy Settings" button', 'ub' ); ?></button>
-				<button type="button" class="sui-modal-close sui-button sui-button-blue pstoolkit-copy-settings-copy-button" data-module="<?php echo esc_attr( $module['module'] ); ?>" data-nonce="<?php echo esc_attr( wp_create_nonce( $dialog_id ) ); ?>" disabled="disabled"><?php echo esc_html_x( 'Copy', 'Dialog "Copy Settings" button', 'ub' ); ?></button>
+				<button type="button" class="sui-button sui-button-ghost" data-modal-close=""><?php echo esc_html_x( 'Abbrechen', 'Dialog "Copy Settings" button', 'ub' ); ?></button>
+				<button type="button" class="sui-modal-close sui-button sui-button-blue pstoolkit-copy-settings-copy-button" data-module="<?php echo esc_attr( $module['module'] ); ?>" data-nonce="<?php echo esc_attr( wp_create_nonce( $dialog_id ) ); ?>" disabled="disabled"><?php echo esc_html_x( 'Kopieren', 'Dialog "Copy Settings" button', 'ub' ); ?></button>
 			</div>
 		</div>
 	</div>
